25396d9a43d5d4204f9aca0c89a88e0fb21fc15f
Ranzer_Dev <rodolfo.antunes9@gmail.com>
2024-09-06 15:55:25 -0300

    pausa para o café

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,5 +1,77 @@
 class RecintosZoo {
 
+    biomas = {
+        1: "savana",
+        2: "floresta",
+        3: "rio"
+    }
+
+    recintosAtuais = [
+        {
+            numero: 1,
+            bioma: biomas[1],
+            tamanhoTotal: 10,
+            animaisExistentens: [animais[3],animais[3],animais[3]],
+        },
+        {
+            numero: 2,
+            bioma: biomas[2],
+            tamanhoTotal: 5,
+            animaisExistentens: [animais[3],animais[3],animais[3]],
+        },
+        {
+            numero: 1,
+            bioma: biomas[1],
+            tamanhoTotal: 10,
+            animaisExistentens: [animais[3],animais[3],animais[3]],
+        },
+        {
+            numero: 1,
+            bioma: biomas[1],
+            tamanhoTotal: 10,
+            animaisExistentens: [animais[3],animais[3],animais[3]],
+        },
+        {
+            numero: 1,
+            bioma: biomas[1],
+            tamanhoTotal: 10,
+            animaisExistentens: [animais[3],animais[3],animais[3]],
+        }
+    ]
+
+    animais = [
+        {
+            animal: "Leão",
+            tamanho: 3,
+            bioma: biomas[1]
+        },
+        {
+            animal: "Leopardo",
+            tamanho: 2,
+            bioma: biomas[1]
+        },
+        {
+            animal: "Crocodilo",
+            tamanho: 3,
+            bioma: biomas[3]
+        },
+        {
+            animal: "Macaco",
+            tamanho: 1,
+            bioma: [biomas[1], biomas[2]]
+        },
+        {
+            animal: "Gazela",
+            tamanho: 2,
+            bioma: biomas[1]
+        },
+        {
+            animal: "Hipopotamo",
+            tamanho: 4,
+            bioma: [biomas[1], biomas[3]]
+        }
+    ]
+
     analisaRecintos(animal, quantidade) {
     }
 
