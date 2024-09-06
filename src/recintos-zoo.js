ff4707ba7e8bbf14cbcada8380106b279a96a55d
Ranzer_Dev <rodolfo.antunes9@gmail.com>
2024-09-06 17:12:49 -0300

    saindo para a faculdade

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -4,40 +4,7 @@ class RecintosZoo {
         1: "savana",
         2: "floresta",
         3: "rio"
-    }
-
-    recintosAtuais = [
-        {
-            numero: 1,
-            bioma: biomas[1],
-            tamanhoTotal: 10,
-            animaisExistentens: [animais[3],animais[3],animais[3]],
-        },
-        {
-            numero: 2,
-            bioma: biomas[2],
-            tamanhoTotal: 5,
-            animaisExistentens: [animais[3],animais[3],animais[3]],
-        },
-        {
-            numero: 1,
-            bioma: biomas[1],
-            tamanhoTotal: 10,
-            animaisExistentens: [animais[3],animais[3],animais[3]],
-        },
-        {
-            numero: 1,
-            bioma: biomas[1],
-            tamanhoTotal: 10,
-            animaisExistentens: [animais[3],animais[3],animais[3]],
-        },
-        {
-            numero: 1,
-            bioma: biomas[1],
-            tamanhoTotal: 10,
-            animaisExistentens: [animais[3],animais[3],animais[3]],
-        }
-    ]
+    };
 
     animais = [
         {
@@ -70,11 +37,51 @@ class RecintosZoo {
             tamanho: 4,
             bioma: [biomas[1], biomas[3]]
         }
-    ]
+    ];
+
+    recintosAtuais = [
+        {
+            numero: 1,
+            bioma: biomas[1],
+            tamanhoTotal: 10,
+            animaisExistentens: [animais[3],animais[3],animais[3]],
+        },
+        {
+            numero: 2,
+            bioma: biomas[2],
+            tamanhoTotal: 5,
+            animaisExistentens: [],
+        },
+        {
+            numero: 3,
+            bioma: [biomas[1],biomas[3]],
+            tamanhoTotal: 7,
+            animaisExistentens: [animais[5]],
+        },
+        {
+            numero: 4,
+            bioma: biomas[3],
+            tamanhoTotal: 8,
+            animaisExistentens: [],
+        },
+        {
+            numero: 5,
+            bioma: biomas[1],
+            tamanhoTotal: 9,
+            animaisExistentens: [animais[1]],
+        }
+    ];
 
     analisaRecintos(animal, quantidade) {
-    }
+        for (var i = 0; i <= animais.length; i++){
 
+            if (animais[i]['animal'] == animal){
+                console.log(animal);
+            } else {
+                return {erro: "Animal inválido"};
+            }
+        }
+    }
 }
 
 export { RecintosZoo as RecintosZoo };
