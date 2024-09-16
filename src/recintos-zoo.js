31acbb8128ad549391134753f6e8f19ccdb22996
Ranzer_Dev <rodolfo.antunes9@gmail.com>
2024-09-15 21:32:40 -0300

    Revisando alguns erros gramaticais

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,5 +1,5 @@
 
-// dionario contendo os biomas possiveis não só os dentro do zoologico
+// Dicionário contendo os biomas possíveis, não só os dentro do zoológico.
 const biomas = {
     1: "savana",
     2: "floresta",
@@ -10,7 +10,7 @@ const biomas = {
     7: "taiga"
 };
 
-//declarendo animais que o zoologico tem condições de cuidar e suas especificações
+//Declarando os animais que o zoológico tem condições de cuidar e suas especificações.
 
 let animais = [
     {
@@ -51,7 +51,7 @@ let animais = [
     }
 ];
 
-//recintos com o bioma que o zoologico tem e seus respectivos habitantes
+//Recintos com o seu respectivo bioma e habitantes
 
 var recintosDoZoologico = [
     {
@@ -86,29 +86,29 @@ var recintosDoZoologico = [
     }
 ];
 
-// analise para criar propriedades unicas para cada recinto
-// dependendo do indivio que cada um comporta 
+// análise para criar propriedades únicas para cada recinto
+// dependendo do indivíduo que cada um comporta
 
 var animaisPorRecinto = [];
 var capacidadeOcupada = [];
 
-//isolando os animais de objeto recinto para variavel de array
+// isolando os animais de objeto recinto para variável de array
 for (let recinto of recintosDoZoologico){
     animaisPorRecinto.push(recinto['animaisNoRecinto']);
 }
 
-//laço de repetição aninhado que itera sobre cada animal no recinto para poder definir o 
-//tamanho disponivel no recinto para receber novos animais
+// laço de repetição aninhado que itera sobre cada animal no recinto para poder definir o
+// tamanho disponível no recinto para receber novos animais
 for (let i = 0; i < animaisPorRecinto.length; i++ ){
 
-    //atribuindo Zeros ao array capacidadeOcupada para ele não ficar vazio
-    //e não retornar NaN quando eu executar os calculos
+        // atribuindo zeros ao array capacidadeOcupada para ele não ficar vazio
+        // e não retornar NaN quando eu executar os cálculos
     for (let a = 0; a < animaisPorRecinto[i].length; a++ ){
         if (capacidadeOcupada[i] == undefined){
             capacidadeOcupada[i] = 0;
         }
         capacidadeOcupada[i] = capacidadeOcupada[i] + animaisPorRecinto[i][a]['tamanho'];
-            //adionando a familia do animal ao recinto (familias de carnivor diferentes tendem a rivalizar)
+           // adicionando a família do animal ao recinto (famílias de carnívoros diferentes tendem a rivalizar)
         if(animaisPorRecinto[i][a]['dieta'] == 'carnivoro' && animaisPorRecinto[i][a]['familiaDoCanivoro'] == 'felideos'){
             recintosDoZoologico[i].dieta = animaisPorRecinto[i][a]['dieta'];
             recintosDoZoologico[i].familia = animaisPorRecinto[i][a]['familiaDoCanivoro'];
@@ -118,10 +118,10 @@ for (let i = 0; i < animaisPorRecinto.length; i++ ){
         }
     }
 } 
-//laço para adicionar a nova propriedade espacoLivre aos objetos recintosDoZoologico do zoologico
+// laço para adicionar a nova propriedade espaçoLivre aos objetos recintosDoZoológico do zoológico
 for (let recinto = 0; recinto < recintosDoZoologico.length; recinto++){
-    //se o recinto não tem nenhum animal o valor retorna como vazio ou undefined
-    //tratamento para um dieta numero, já que ele vai guardar um dado numerico
+    // se o recinto não tem nenhum animal o valor retorna como vazio ou undefined
+    // tratamento para um dieta número, já que ele vai guardar um dado numérico
     if(capacidadeOcupada[recinto] == undefined){
         capacidadeOcupada[recinto] = 0;
     }
@@ -148,8 +148,8 @@ class RecintosZoo {
             if (quantidade < 1) {
                 return {erro: "Quantidade inválida"};
             }
-            //caso o zoologico aceite o animal informado ele atribui alguns dados adicionais
-            //e simplifica alguns já existentes
+            // verifica se o animal adicionado ao parâmetro animal da função
+            // é igual a algum animal que o zoológico aceita no momento
             if (animal == 'LEAO') {
                 animal = animais[0];
                 tamanhoDosAnimais = animal['tamanho'] * quantidade;
@@ -187,32 +187,32 @@ class RecintosZoo {
                 dietaDoAnimal = animal['dieta'];
             }
     
-            //esses laços aninhado definem para qual recinto cada animal vai 
-            //de acordo com o exigido de cada um
+            // esses laços aninhados definem para qual recinto cada animal vai
+            // de acordo com o exigido de cada um
             for (let i = 0; i < recintosDoZoologico.length; i++) {
-                //verifica qual dos 5 recintos tem espaço disponivel para a quantidade informada
+                // verifica qual dos 5 recintos tem espaço disponível para a quantidade informada
                 if (recintosDoZoologico[i]['espacoLivre'] > tamanhoDosAnimais) {
-                    //itera sobre cada bioma do Recinto
+                   // itera sobre cada bioma do Recinto
                     for (let a = 0; a < recintosDoZoologico[i]['bioma'].length; a++) {
-                        //itera sobre cada bioma do Animal
+                        // itera sobre cada bioma do Animal
                         for (let b = 0; b < habitats.length; b++) {
-                            //essa comparação verifica se os dois laços acima são iguais
-                            //se a dieta do animal é a mesma que a do recinto
-                            //ou se o tem todo o espaço livre (se for ocupado e a dieta for carnivora ele procura um vazio)
+                            // essa comparação verifica se os dois laços acima são iguais
+                            // se a dieta do animal é a mesma que a do recinto
+                            // ou se o tem todo o espaço livre (se for ocupado e a dieta for carnívora ele procura um vazio)
                             if (recintosDoZoologico[i]['bioma'][a] == habitats[b] && (dietaDoAnimal == recintosDoZoologico[i]['dieta'] || recintosDoZoologico[i]['espacoLivre'] - recintosDoZoologico[i]['espacoTotal'] == 0)) {
-                                //se recinto é compativel adiciono a quantidade de animais especificada ao recinto
+                                // se o recinto é compatível adiciono a quantidade de animais especificada ao recinto
                                 for (let c = 0; c < quantidade; c++) {
-                                    //Macaco não vive sozinho, se tem algum animal no recinto adicona macaco, 
-                                    //ou se forem dois macacos adicona macaco
-                                    //se não for macaco, só adiciona
+                                    // Macaco não vive sozinho, se tem algum animal no recinto adiciona macaco,
+                                    // ou se forem dois macacos adiciona macaco
+                                    // se não for macaco, só adiciona
                                     if (recintosDoZoologico[i]['animaisNoRecinto'].length != 0 || animal['animal'] == "MACACO" && tamanhoDosAnimais > 1) {
                                         recintosDoZoologico[i]['animaisNoRecinto'].push(animal);
                                     } else {
                                         recintosDoZoologico[i]['animaisNoRecinto'].push(animal);
                                     }
                                 }
-                                //se o recinto tem mais de uma especie tira um espaço adicional, caso não se
-                                //for vazio não tira nada, caso contrario tira só o espaço que o animal ocupa
+                                // se o recinto tem mais de uma espécie tira um espaço adicional, caso não se
+                                // for vazio não tira nada, caso contrário tira só o espaço que o animal ocupa
                                 especiesDiferentes[i] = recintosDoZoologico[i]['animaisNoRecinto'].some(elemento => elemento.animal != animal['animal']);
                                 if (especiesDiferentes[i] == true) {
                                     recintosViaveis.push(`Recinto ${recintosDoZoologico[i]['numero']} (espaço livre: ${recintosDoZoologico[i]['espacoLivre'] - tamanhoDosAnimais - 1} total: ${recintosDoZoologico[i]['espacoTotal']})`);
